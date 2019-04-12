778ec8b8ff91a83d03366a964648f5c86b836918
Andrew Chow <andrewcxw@gmail.com>
2019-04-12 02:21:25 +0100

    Include endpoints for the scheduler

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import json
 
 from flask import Flask, jsonify, request, abort
 from Routing.Pathfinder import Pathfinder
+from Scheduling.Scheduler import FCFSScheduler
 
 app = Flask(__name__)
 
+# run the scheduler
+s = FCFSScheduler()
+s.start()
 
 @app.route('/')
 def home():
@@ -19,6 +23,20 @@ def routes():
         return jsonify(r)
     else:
         return abort(400)
+
+
+@app.route('/jobs', methods=['POST'])
+def jobs():
+    if request.is_json:
+        app.logger.info(json.dumps(request.json))
+        return s.getJob(1)
+    else:
+        return abort(400)
+
+
+@app.route('/jobs_dummy')
+def jobs_dummy():
+    return s.getJob(1)
 
 
 if __name__ == '__main__':
