88f72eac47097605b87aa9595fd31a7d4997c932
Andrew Chow <andrewcxw@gmail.com>
2019-05-14 20:04:27 +0100

    Ensure that POST requests are JSON

--- app.py
+++ app.py
@@ -20,34 +20,36 @@ def home():
 
 @app.route('/routes', methods=['GET', 'POST'])
 def routes():
-    global written
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_json:
         data = request.get_json()
         return pathfinder.getRoute(data), 200
     elif request.method == 'GET':
-        return jsonify({'success': 'true'}), 200
+        return jsonify({'success': True}), 200
     else:
         return abort(400)
 
 
 @app.route('/jobs', methods=['GET', 'POST'])
 def jobs():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_json:
         data = request.get_json()
         return scheduler.getJob(data), 200
     elif request.method == 'GET':
-        return jsonify({'success': 'true'}), 200
+        return jsonify({'success': True}), 200
     else:
         return abort(400)
 
 
-@app.route('/update_timescale', methods=['POST'])
+@app.route('/update_timescale', methods=['GET', 'POST'])
 def update_timescale():
-    if request.is_json:
+    if request.method == 'POST' and request.is_json:
         data = request.get_json()
         app.logger.debug(json.dumps(data))
         scheduler.updateTimescale(data['timescale'])
-        return jsonify({'success': 'true'}), 200
+        return jsonify({'success': True,
+                        'timescale': scheduler.generator.timescale}), 200
+    elif request.method == 'GET':
+        return jsonify({'success': True}), 200
     else:
         return abort(400)
 
